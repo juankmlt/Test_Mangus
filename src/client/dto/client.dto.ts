85f1b9c60414ce280f0bd49d425b1407ea7fdfcb
juankmlt <juan.aldana@correounivalle.edu.co>
2021-09-23 00:02:48 -0500

    finishing_items

--- src/client/dto/client.dto.ts
+++ src/client/dto/client.dto.ts
@@ -1,7 +1,11 @@
 
-export class ClientDto {
+import { UserDto } from "src/user/dto/user.dto";
+
+export class ClientDto extends UserDto{
     name? : string;
     lastname? : string;
     address? : string;
     phone? : number;
+    username?: string;
+    password?: string;
 }
